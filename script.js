582891de9af7a96a3a7de3345f42fd3a162f1c0c
Fujinjakka <calvieri.marco@gmail.com>
2020-11-11 21:07:49 +0100

    programma completato
    manca un po di stile

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 
-// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km);
-// va applicato uno sconto del 20% per i minorenni;
-// va applicato uno sconto del 40% per gli over 65.
+function activebutton() {
+  var eta = prompt("quanti anni hai?");
+  console.log("eta del passeggero: " + eta);
 
+  var kilometri = prompt("quanti kilometri vuoi fare?");
+  console.log("kilometri da percorrere: "+ kilometri);
 
-
-// chiedere eta e chilometri
-
-// calcolare il prezzo del Biglietto
-
-var eta = prompt("quanti anni hai?");
-console.log("eta del passeggero " + eta);
-var kilometri = prompt("quanti kilometri vuoi fare?");
-console.log("kilometri da percorrere "+ kilometri);
-var scontoMin = (biglietto * 100) / 20;
-console.log(scontoMin);
-var scontoMag = (biglietto * 100) / 40;
-console.log(scontoMag);
-
-
-if ((eta > 18) && (eta > 64)); {
   var biglietto = (kilometri * 0.21);
-  console.log(biglietto);
-} else if (eta < 18); {
-  var biglietto = ((kilometri * 0.21) - (scontoMin));
-  console.log(biglietto);
-} else if (eta > 64); {
-  var biglietto = ((kilometri * 0.21) - (scontoMin));
-  console.log(biglietto);
+  console.log("costo del biglietto: " + biglietto);
+
+  var scontoMin = (biglietto * 20) / 100;
+  console.log("sconto minorenni: " + scontoMin);
+
+  var scontoMax = (biglietto * 40) / 100;
+  console.log("sconto over 65: " + scontoMax);
+
+
+  if (eta >= 18 && eta <= 65) {
+    document.getElementById('result').innerHTML = ("il tuo biglietto è di " + biglietto + "euro");
+    console.log(result);
+  } else if (eta < 18) {
+    document.getElementById('result').innerHTML = ("il tuo biglietto è di " + (biglietto - scontoMin) + "euro");
+    console.log(result);
+  } else if (eta > 65) {
+    document.getElementById('result').innerHTML = ("il tuo biglietto è di " + (biglietto - scontoMax) + "euro");
+    console.log(result);
+  }
 }
